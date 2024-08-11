7ae7da093bbb01585aacd3561973c584ff60c55c
sinan <sinan@SINAN-SENKUL>
2024-08-12 02:54:55 +0300

    Add project files.

---
+++ Inheritance/BasePerson.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Xml.Linq;
+
+public class BasePerson
+{
+    string Name { get; set; }
+    string LastName { get; set; }
+    public void NameLog()
+    {
+        Console.WriteLine($"name: {Name} lastname: {LastName}");
+    }
+}
+
+//derived class
+public class Student : BasePerson
+{
+    string Name { get; set; }
+    string LastName { get; set; }
+    int StudentNumber { get; set; }
+    public Student(string name, string lastName, int studentNumber)
+    {
+        Name = name;
+        LastName = lastName;
+        StudentNumber = studentNumber;
+    }
+    public void StudentLog()
+    {
+        Console.WriteLine($"student's number: {StudentNumber} name: {Name} lastname: {LastName}");
+    }
+}
+
+public class Teacher : BasePerson
+{
+    string Name { get; set; }
+    string LastName { get; set; }
+    int Salary { get; set; }
+    public Teacher(string name, string lastName, int salary)
+    {
+        Name = name;
+        LastName = lastName;
+        Salary = salary;
+    }
+    public void TeacherLog()
+    {
+        Console.WriteLine($"teacher's name: {Name} lastname: {LastName} salary:{Salary}");
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Ali = new Student("ali", "veli", 1001);
+            Ali.StudentLog();
+            Teacher Ayşe = new Teacher("ayşe", "mayşe", 25000);
+            Ayşe.TeacherLog();
+        }
+    }
+}
