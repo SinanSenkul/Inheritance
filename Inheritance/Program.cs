ccd4c084af64f06edeac71416d6536c880ba1653
sinan senkul <sinan@sinan-senkul.local>
2024-08-12 12:39:54 +0300

    namelog method used in program class

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -7,9 +7,12 @@ namespace Inheritance
         static void Main(string[] args)
         {
             Student Ali = new Student("ali", "veli", 1001);
-            Ali.StudentLog();
+            Ali.NameLog(); //base class method
+            Ali.StudentLog(); //student class method
             Teacher Ayşe = new Teacher("ayşe", "mayşe", 25000);
+            Ayşe.NameLog();
             Ayşe.TeacherLog();
+            
         }
     }
 }
